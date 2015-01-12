0e67a6a5ed0c7bf5183213e2c2d27a1f0b8787b9
Terence Williams <wterence73@gmail.com>
2015-01-11 21:04:21 -0500

    Update 6

--- Wk1/goal1_basics/js/Js/main.js
+++ Wk1/goal1_basics/js/Js/main.js
@@ -25,12 +25,35 @@ console.log("FIGHT!!!");
     function fight(){
         console.log('in the fight function');
 
+        alert(playerOneName+":"+playerOneHealth+" *START* "+playerTwoName+":"+playerTwoHealth);
+
+   for (var i=0; i < 10; i++){
+       console.log(i);
+
+       //random formula created Math.floor(Math.random() * (max - min) + min);
+
+       var minDamage1 = player1Damage * .5;
+       var minDamage2 = player2Damage * .5;
+       var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
+       var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+
+       //console.log(f1);
+       //console.log(f2);
+
+       //inflict damage
+       playerOneHealth-=f1;
+       playerTwoHealth-=f2;
+
+
+
+
+   };
     };
 
 
     function winnerCheck(){
 
-        
+
     };
 
     /***** The program gets started below *****/
