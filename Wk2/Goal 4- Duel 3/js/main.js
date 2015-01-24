15c5bd58352fa8ca4d11bd34da027388425773b5
Terence Williams <wterence73@gmail.com>
2015-01-23 22:46:06 -0500

    Goal 4- Duel 3
    Update 11

--- Wk2/Goal 4- Duel 3/js/main.js
+++ Wk2/Goal 4- Duel 3/js/main.js
@@ -12,69 +12,73 @@ console.log("FIGHT!!!");
 
     // New game objects and arrays created
 
-    var playerOne = {name: "Kabal", damage: "20", health: "100"};
-    var playerTwo = {name: "Kratos", damage: "20", health: "100"};
-   
+    var fighters = [
+        playerOne = {name: "Kabal", damage: "20", health: "100"},
+        playerTwo = {name: "Kratos", damage: "20", health: "100"}
 
-    var round=0;
+        ];
+
+
+    var round = 0;
 
 
     // Fight button variable created
 
-    var fightButton = document.getElementById(".buttonblue");
+    var fightButton = document.getElementById("buttonblue");
 
 
      // players health from DOM
 
-    document.getElementById("playerOne").innerHTML = playerOne.name + ":" + playerOne.health;
-    document.getElementById("playerTwo").innerHTML = playerTwo.name + ":" + playerTwo.health;
+    document.getElementById("playerOne").innerHTML = playerOne.name + ": " + playerOne.health;
+    document.getElementById("playerTwo").innerHTML = playerTwo.name + ": " + playerTwo.health;
 
 
      // New fight function created
 
-    function fight(){
+    function fight() {
 
         if (playerOne.health < 1 && playerTwo.health < 1) {
             return false;
         }
 
-       //random formula created Math.floor(Math.random() * (max - min) + min);
+        //random formula created Math.floor(Math.random() * (max - min) + min);
 
-       var minDamage1 = playerOne.damage * .5;
-       var minDamage2 = playerTwo.damage * .5;
-       var f1 = Math.floor(Math.random()*(playerOne.damage-minDamage1)+minDamage1);
-       var f2 = Math.floor(Math.random()*(playerTwo.damage-minDamage2)+minDamage2);
+        var minDamage1 = playerOne.damage * .5;
+        var minDamage2 = playerTwo.damage * .5;
+        var f1 = Math.floor(Math.random() * (playerOne.damage - minDamage1) + minDamage1);
+        var f2 = Math.floor(Math.random() * (playerTwo.damage - minDamage2) + minDamage2);
 
-       //console.log(f1);
-       //console.log(f2);
+        //console.log(f1);
+        //console.log(f2);
 
-       //inflict damage
-       playerOne.health - f1;
-       playerTwo.health - f2;
+        //inflict damage
+        playerOne.health = playerOne.health - f1;
+        playerTwo.health = playerTwo.health - f2;
 
-       var results = winnerCheck();
-       console.log(results);
+        var results = winnerCheck();
 
-       if (results === "no winner"){
-          round++;
-           document.getElementById("playerOne").innerHTML = playerOne.name + ":" + playerOne.health;
-           document.getElementById("playerTwo").innerHTML = playerTwo.name + ":" + playerTwo.health;
-           document.getElementById("round").innerHTML = "round " + round + " : Finished!";
-       }else{
-           document.getElementById("playerOne").innerHTML = "";
-           document.getElementById("playerTwo").innerHTML = "";
-           document.getElementById("round").innerHTML = results;
+        console.log(results);
 
-       }
+        if (results === "no winner") {
+            round++;
+            document.getElementById("playerOne").innerHTML = playerOne.name + ":" + playerOne.health;
+            document.getElementById("playerTwo").innerHTML = playerTwo.name + ":" + playerTwo.health;
+            document.getElementById("round").innerHTML = "round " + round + " : Finished!";
+        } else {
+            document.getElementById("playerOne").innerHTML = "";
+            document.getElementById("playerTwo").innerHTML = "";
+            document.getElementById("round").innerHTML = results;
 
-   };
-        console.log(button);
-    button.innerHTML = "Fatality!!!";
-    button.setAttribute('onclick');
+            console.log(button);
+            button.innerHTML = "Fatality!!!";
+            button.setAttribute('onclick', null);
+
+        }
+
+    };
 
     function winnerCheck() {
         console.log("in winnerCheck FN");
-
 
         var result = "no winner";
 
@@ -86,22 +90,16 @@ console.log("FIGHT!!!");
             result = playerOne.name + "WINS!!!";
         }
 
-        return results;
+        return result;
 
     };
 
 
-        button.onclick = function(e) {
+        fightButton.onclick = function(e) {
             fight();
             e.preventDefault();
-
             return false;
 
     }
-
-    /***** The program gets started below *****/
-    console.log('program starts');
-    fight();
-
 
 })();
