d1205494b39dce5917ae6c40e24dca2eadc4a444
Terence Williams <wterence73@gmail.com>
2015-01-29 21:52:03 -0500

    Goal 7 Assign Objects
    Start of project

---
+++ Wk4/goal7_assign_objects/Goal 7 Assign Objects/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Terence Williams
+ * Class: PWA 1
+ * Goal: Goal 7 Assign Objects
+ * 01/29/15
+ */
+
+
+// Array created for project names
+
+var projectNames = ["Paul", "Paula", "Pam", "Pierre", "Precious"];
+
