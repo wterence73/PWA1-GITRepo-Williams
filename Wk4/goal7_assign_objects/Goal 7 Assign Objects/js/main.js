3f14d839c20494c21956b5dbfba19e5989a5aef3
Terence Williams <wterence73@gmail.com>
2015-01-30 23:39:17 -0500

    Goal 7 pseudo complete
    Finished

--- Wk4/goal7_assign_objects/Goal 7 Assign Objects/js/main.js
+++ Wk4/goal7_assign_objects/Goal 7 Assign Objects/js/main.js
@@ -6,10 +6,49 @@
  */
 
 
-// Array created for project names
 
-var projectNames = ["Paul", "Paula", "Pam", "Pierre", "Precious"];
+// Create two files
+     // main.js creation
+     // person.js creation
 
-// For loop for the object people
+// Create an self-executing function
 
-for 
+// Create an Array called names
+      // five names entered in array that will randomly be chosen
+
+// Three instances where the person object calls the person
+    // for loop created
+
+// document.getid used thrice
+
+// Math.random used to have randomly chosen person chosen
+
+// Create function that calls person and list jobs
+
+// Must be created to call random name once
+
+// Interval setup created to display how often to change
+
+// Run update function created
+
+// All inputs for main.js completed
+
+
+
+// person object added to window object in person.js
+
+// Variables created for person.job
+   // Four jobs created within an array
+
+// Person constructor created with four outputs
+
+// Person function created to provide update
+
+// Project complete
+
+
+
+
+
+
+
